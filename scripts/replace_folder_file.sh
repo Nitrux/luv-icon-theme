aa20ef1b8f385d6e4cb25fd2d3002aaeaf999397
Uri <uri@korora.nitrux>
2014-01-31 20:20:14 -0600

    Re-uploaded everything

---
+++ scripts/replace_folder_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#This will replace the colors in a folder svg file. The one hack is that to change the foreground 
+#shape colors you must provide the old color and this color cannot be repeated anywhere that you do
+#not want it to change. The folder back, paper, and front colors are precise replacements in
+#xml. However, the shape color replacement is a regular expression search/replace. To change the
+#shape color again, you must replace the original value with the new value and run it again. 
+
+backColor=34495d
+frontColor=1abc9c
+paperColor=ffffff
+shapeColorOriginal=34495e
+shapeColorNew=304050
+
+xsltproc --stringparam backColor $backColor --stringparam frontColor $frontColor --stringparam paperColor $paperColor change_folder_colors.xslt $1 > tmp.svg && mv tmp.svg $1
+sed -i "s/#$shapeColorOriginal;/#$shapeColorNew;/g" $1
