afe4f4d744df2fcc7a0492d8a0a3cff24341ca15
NBonaparte <98007b33@opayq.com>
2015-05-02 20:03:19 -0700

    Fixed broken images.

--- actions/22/changecolor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for f in *.svg; 
-do 
-  echo "Changing color of $f ..." 
-  sed -i -e's/\"#.\{6\}/\"#000000/g' -e 's/#.*;/#ffffff;/g' "$f";
-  sed -i "s/<path/<path fill=\"#ffffff\"/" "$f";
-done
