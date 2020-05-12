026886e1ac98557badc628b0f074334a518f17c9
Uri Herrera <hello@uriherrera.com>
2020-05-11 19:35:44 -0500

    [ci skip]

--- travis-build.sh
+++ travis-build.sh
@@ -5,6 +5,7 @@ apt-get --yes install wget gnupg2
 
 ### Add KDENeon Repository
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 55751E5D
+wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/nitrux-iso-tool/development/configs/files/sources.list.neon.user
 
 ### Install Dependencies
 apt-get --yes update
