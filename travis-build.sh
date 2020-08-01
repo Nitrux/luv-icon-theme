ce151eed13f60dc0055960843ee58da6cafc4cce
Uri Herrera <hello@uriherrera.com>
2020-08-01 05:49:20 -0500

    updat travis build script and config

--- travis-build.sh
+++ travis-build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-apt-get --yes update
-apt-get --yes install wget gnupg2
-
-### Add KDENeon Repository
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 55751E5D
-wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/nitrux-iso-tool/development/configs/files/sources.list.neon.user
+apt -qq update
+apt -qq -yy install equivs curl git
 
 ### Install Dependencies
-apt-get --yes update
-apt-get --yes dist-upgrade
-apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs
+apt -qq -yy dist-upgrade
+apt -qq -yy install devscripts lintian build-essential automake autotools-dev equivs
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
